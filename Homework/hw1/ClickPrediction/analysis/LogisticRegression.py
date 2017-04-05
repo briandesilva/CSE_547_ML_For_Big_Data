368e33a08ad1aa003ced786cb3cff9fca6d0ef6f
Brian de Silva <bdesilva@uw.edu>
2017-04-04 19:19:41 -0700

    Fixed some homework problems. Got Vanilla logistic regression working.

--- Homework/hw1/ClickPrediction/analysis/LogisticRegression.py
+++ Homework/hw1/ClickPrediction/analysis/LogisticRegression.py
@@ -1,7 +1,9 @@
 import math
+import matplotlib.pyplot as plt
 
 from analysis.DataSet import DataSet
 from util.EvalUtil import EvalUtil
+from analysis.BasicAnalysis import BasicAnalysis
 
 # This class represents the weights in the logistic regression model.
 class Weights:
@@ -49,8 +51,8 @@ class LogisticRegression:
   # ==========================
   def compute_weight_feature_product(self, weights, instance):
     # First few components of inner product
-    output =  1.0 * (weights.w0 + weights.age * instance.age + weights.depth * instance.depth
-      + weights.position * instance * position + weights.gender * instance.gender)
+    output =  1.0 * (weights.w0 + weights.w_age * instance.age + weights.w_depth * instance.depth
+      + weights.w_position * instance.position + weights.w_gender * instance.gender)
     # Components of inner product involving tokens
     for token in instance.tokens:
       if (token in weights.w_tokens):
@@ -82,32 +84,53 @@ class LogisticRegression:
     # Number of steps to perform between each update
     output_steps = 100
 
+    # Variable to record the running loss
+    loss = 0.0
+
     # For each data point:
     while dataset.hasNext():
       instance = dataset.nextInstance()
 
       # Your code: perform delayed regularization
 
-      # Your code: predict the label, record the loss
-      if (mod(dataset.counter,output_steps)==0 and dataset.counter > 0) or (!dataset.hasNext()):
+      # Predict the label, record the loss
+      ip = self.compute_weight_feature_product(weights,instance)
+      if ip > 0:
+        prediction = 1.
+      else:
+        prediction = 0.
+
+      loss += pow(prediction - instance.clicked, 2)
+
+
+      if ((dataset.counter % output_steps)==0) or (not dataset.hasNext()):
         if dataset.hasNext():
-          (predictions,avg_loss[dataset.counter / output_steps]) = self.predict(weights,dataset)
-          print "Average loss after %d iterations: %f" %(dataset.counter, avg_loss[dataset.counter / output_steps])
+          # (predictions,loss) = self.predict(weights,dataset)
+          avg_loss[(dataset.counter / output_steps) - 1] = loss / dataset.counter
+          # print "Average loss after %d iterations: %f" %(dataset.counter, loss / dataset.counter)
         else:
-          (predictions,avg_loss[(dataset.counter/output_steps) + 1]) = self.predict(weights,dataset)
-          print "Average loss after %d iterations: %f" %(dataset.counter, avg_loss[(dataset.counter/output_steps)+1])
+          # (predictions,loss) = self.predict(weights,dataset)
+          avg_loss[dataset.counter / output_steps] = loss / dataset.counter
+          # print "Average loss after %d iterations: %f" %(dataset.counter, loss / dataset.counter)
 
         
       # Compute w0 + <w, x>, and gradient
-      exp_inner_product = math.exp(self.compute_weight_feature_product(weights, instance))
+      exp_inner_product = math.exp(ip)
       coeff = instance.clicked - exp_inner_product / (1 + exp_inner_product)
       
       # Update weights along the negative gradient
-      weights.w_age -= step * instance.age * coeff
-      weights.w_gender -= step * instance.gender * coeff
-      weights.w_depth -= step * instance.depth * coeff
-      weights.w_position -= step * instance.position * coeff
-      weights.w_w0 -= step * coeff
+      weights.w_age += step * instance.age * coeff
+      weights.w_gender += step * instance.gender * coeff
+      weights.w_depth += step * instance.depth * coeff
+      weights.w_position += step * instance.position * coeff
+      weights.w0 += step * coeff
+
+      # Update weights corresponding to tokens
+      for token in instance.tokens:
+        if token in weights.w_tokens:
+          weights.w_tokens[token] += step * coeff
+        else:
+          weights.w_tokens[token] =  step * coeff
 
       
     dataset.reset()
@@ -119,55 +142,97 @@ class LogisticRegression:
   # @param dataset {DataSet}
   # ==========================
   def predict(self, weights, dataset):
+    # exp_inner_product = math.exp(self.compute_weight_feature_product(weights,instance))
+    # return exp_inner_product / (1 + exp_inner_product)
+
+
     predictions = [None] * dataset.size
     count = dataset.counter             # Record position in dataset so it can be returned the same way
-    loss = 0.0                          # Compute loss as we go if we are looking at the training set
 
-    # Check if we're working with training data
-    if dataset.has_label:
-      # Get predictions for the later parts of dataset
-      while dataset.hasNext():
-        instance = dataset.nextInstance()
-        exp_inner_product = math.exp(self.compute_weight_feature_product(weights,instance))
-        predictions[dataset.counter] = exp_inner_product / (1 + exp_inner_product)
-        loss += pow(predictions[dataset.counter] - instance.clicked,2)
+    # # Check if we're working with training or testing data
+    # if dataset.has_label:
+    #   # Get predictions for the later parts of dataset
+    #   while dataset.hasNext():
+    #     instance = dataset.nextInstance()
+    #     exp_inner_product = math.exp(self.compute_weight_feature_product(weights,instance))
+    #     predictions[dataset.counter-1] = exp_inner_product / (1 + exp_inner_product)
+    #     loss += pow(predictions[dataset.counter-1] - instance.clicked,2)
 
-      # Get predictions for the earlier part of the dataset
-      dataset.reset()
-      while dataset.counter < count:
-        instance = dataset.nextInstance()
-        exp_inner_product = math.exp(self.compute_weight_feature_product(weights,instance))
-        predictions[dataset.counter] = exp_inner_product / (1 + exp_inner_product)
-        loss += pow(predictions[dataset.counter] - instance.clicked,2)
+    #   # Get predictions for the earlier part of the dataset
+    #   dataset.reset()
+    #   while dataset.counter < count:
+    #     instance = dataset.nextInstance()
+    #     exp_inner_product = math.exp(self.compute_weight_feature_product(weights,instance))
+    #     predictions[dataset.counter-1] = exp_inner_product / (1 + exp_inner_product)
+    #     loss += pow(predictions[dataset.counter-1] - instance.clicked,2)
 
-    else:
-      # Get predictions for the later parts of dataset
-      while dataset.hasNext():
-        instance = dataset.nextInstance()
-        exp_inner_product = math.exp(self.compute_weight_feature_product(weights,instance))
-        predictions[dataset.counter] = exp_inner_product / (1 + exp_inner_product)
-
-      # Get predictions for the earlier part of the dataset
-      dataset.reset()
-      while dataset.counter < count:
-        instance = dataset.nextInstance()
-        exp_inner_product = math.exp(self.compute_weight_feature_product(weights,instance))
-        predictions[dataset.counter] = exp_inner_product / (1 + exp_inner_product)
+    # else:
 
 
-    return (predictions,loss / count)
+    # Get predictions for the later parts of dataset
+    while dataset.hasNext():
+      instance = dataset.nextInstance()
+      exp_inner_product = math.exp(self.compute_weight_feature_product(weights,instance))
+      predictions[dataset.counter-1] = exp_inner_product / (1 + exp_inner_product)
+
+    # Get predictions for the earlier part of the dataset
+    dataset.reset()
+    while dataset.counter < count:
+      instance = dataset.nextInstance()
+      exp_inner_product = math.exp(self.compute_weight_feature_product(weights,instance))
+      predictions[dataset.counter-1] = exp_inner_product / (1 + exp_inner_product)
+
+
+    return predictions
   
   
 if __name__ == '__main__':
   TRAININGSIZE = 2335859
   TESTINGSIZE = 1016552
 
+  # TRAININGSIZE = 10001
+  # TESTINGSIZE = 10001
 
   # Hyperparameters:
-  lambduh = 0           # Regularization parameter
-  step = 1.e-3               # SGD stepsize
+  lambduh = 0                 # Regularization parameter
+  step = 1.e-3                # SGD stepsize
 
   training = DataSet("../../data/train.txt", True, TRAININGSIZE)
   avg_loss = [None] * (TRAININGSIZE / 100 + 1)
-  print "Training Logistic Regression..."
 
+  # Train the logistic regression model
+  l = LogisticRegression()
+  print "Training Logistic Regression..."
+  weights = l.train(training,lambduh,step,avg_loss)
+  print "Done.\nRMSE on the training set: %f" %avg_loss[-1]
+  print "l2 norm of weights: %f" %weights.l2_norm()
+
+  # # Get the RMSE on the test data using logistic regression
+  # testing = DataSet("../../data/test.txt", False, TESTINGSIZE)
+  # # (test_predictions, dummy) = l.predict(weights,testing)
+  # test_predictions = l.predict(weights,testing)
+  # # count = 0
+  # # while testing.hasNext():
+  # #   instance = testing.nextInstance()
+  # #   test_predictions[count] = l.predict(weights, instance)
+  # #   count += 1
+
+
+  # evaluator = EvalUtil()
+  # test_rmse = evaluator.eval("../../data/test_label.txt",test_predictions)
+  # print "RMSE on the test set using logistic regression: %f" %test_rmse
+
+  # # Get RMSE on the test data using baseline
+  # analyze = BasicAnalysis("../../data/test_label.txt",test_avg_ctr)
+  # test_avg_ctr = analyze.average_ctr(testing)
+  # test_avg_rmse = evaluator.eval_baseline()
+  # print "RMSE on the tes set using average CTR: %f" %test_avg_rmse
+
+  # Plot the error as a function of iterations
+  its = range(100,TRAININGSIZE,100)
+  its.append(TRAININGSIZE)
+  plt.plot(its,avg_loss)
+  plt.xlabel("Steps")
+  plt.ylabel("Average loss")
+  plt.title("Average Loss During Logistic Regression Training (eta=0.001)")
+  plt.show()
