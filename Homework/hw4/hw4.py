5c404ce0f0879c0cd5d17b5ba2311269de8b1873
Brian de Silva <bdesilva@uw.edu>
2017-06-01 17:08:43 -0700

    Problem 2 almost done.

--- Homework/hw4/hw4.py
+++ Homework/hw4/hw4.py
@@ -27,6 +27,7 @@ class ThompsonSampling():
 		self.failures     = np.ones(self.k)
 		self.reward       = 0
 		self.avg_regret   = np.ones(self.T) * max(self.probs)
+		self.N 			  = np.zeros((k,self.T))
 
 	# Function to choose an arm, pull it, and update its posterior
 	def pull_arm(self):
@@ -41,6 +42,9 @@ class ThompsonSampling():
 				a  = i
 
 		# Pull arm and update posterior/reward
+		if self.t > 1:
+			self.N[:,self.t-1] = self.N[:,self.t-2]
+		self.N[a,self.t-1] += 1
 		r = np.random.binomial(1,self.probs[a])
 		if r == 1:
 			self.successes[a] += 1
@@ -61,6 +65,17 @@ class ThompsonSampling():
 	def get_var(self):
 		return (self.successes * self.failures) / ((self.successes + self.failures + 1.) * (self.successes + self.failures)**2)
 
+	# Plot the fraction of the time spent on each arm
+	def plot_N(self):
+		plt.figure()
+		t = np.arange(1,self.T+1)
+		for a in xrange(self.k):
+			plt.plot(t,self.N[a,:] / t,label="Arm " + str(a+1))
+		plt.xlabel('t')
+		plt.ylabel('$N_{a,t}/t$')
+		plt.title('Fraction of time spent on each arm')
+		plt.legend()
+		plt.savefig("figures/N_at.png")
 
 	# Run the actual Thompson sampling scheme
 	def run(self):
@@ -70,6 +85,8 @@ class ThompsonSampling():
 
 			if tt+1 in self.output_times:
 				self.plot_ci()
+
+		self.plot_N()
 
 
 
@@ -104,14 +121,13 @@ class ThompsonSampling():
 # Parameters
 k            = 5	                         	# Number of arms (each with Bernoulli(p) rewards)
 probs        = [1/6., 1/2., 2/3., 3/4., 5/6.]	# Probabilities for each arm
-T            = 1000                            	# Number of iterations
+T            = 10000                            	# Number of iterations
 output_times = [5, 25, 500, 1000]
 
 
 # Perform the Sampling
 TS = ThompsonSampling(k,probs,T,output_times)
 TS.run()
-
 
 # Plot the average regret
 plt.figure()
@@ -120,9 +136,28 @@ plt.xlabel('t')
 plt.ylabel('Average regret')
 plt.title('Average regret')
 plt.savefig('figures/avg_regret.png')
-
-
-
-
-
 # plt.show()
+
+
+# Determine the first time t where N_{5,t}/t is above 0.95 and it
+# stays above it for at least 10 steps in a row
+inds = np.nonzero(TS.N[4,:] / np.arange(1,T+1) > 0.95)[0]
+
+count = 0
+start = 0
+doneFlag = False
+for i in xrange(len(inds)-1):
+	start = inds[i]
+	count = 0
+	while inds[i] == inds[i+1] - 1:
+		count += 1
+		i += 1
+		if count == 10:
+			doneFlag = True
+			break
+	if doneFlag:
+		break
+if doneFlag:
+	print "First time: t = %d" %(start + 1)
+else:
+	print "No first time."
